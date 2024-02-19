e9ae2a80e1b76a650453c5fc1408ae8cb7a54bed
梁小憨 <84164628+LiangXiaohan506@users.noreply.github.com>
2024-02-19 14:17:04 +0800

    Update TCN_util.py

--- utils/TCN_util.py
+++ utils/TCN_util.py
@@ -33,34 +33,44 @@ class Chomp1d(nn.Module):
 
 #%%
 class TemporalBlock(nn.Module):
-    def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2, bias=False, WeightNorm=False, max_norm=1.):
+    def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2, bias=False, WeightNorm=False, group=True, max_norm=1.):
         super(TemporalBlock, self).__init__()
-        self.conv1 = Conv1dWithConstraint(n_inputs, n_outputs, kernel_size, stride=stride, padding=padding, 
-                                          dilation=dilation, bias=bias, doWeightNorm=WeightNorm, max_norm=max_norm)
+        if group:
+            if n_inputs >= n_outputs:
+                self.conv1 = Conv1dWithConstraint(n_inputs, n_outputs, kernel_size, stride=stride, padding=padding, 
+                                                  dilation=dilation, bias=bias, doWeightNorm=WeightNorm, max_norm=max_norm, groups=n_outputs)
+            else:
+                self.conv1 = Conv1dWithConstraint(n_inputs, n_outputs, kernel_size, stride=stride, padding=padding, 
+                                                  dilation=dilation, bias=bias, doWeightNorm=WeightNorm, max_norm=max_norm, groups=n_inputs)
+            self.conv1_point = Conv1dWithConstraint(n_outputs, n_outputs, kernel_size=1, stride=1, bias=bias)
+        else:
+            self.conv1 = Conv1dWithConstraint(n_inputs, n_outputs, kernel_size, stride=stride, padding=padding, 
+                                              dilation=dilation, bias=bias, doWeightNorm=WeightNorm, max_norm=max_norm)
         self.chomp1 = Chomp1d(padding)
         self.bn1 = nn.BatchNorm1d(num_features=n_outputs)
-        self.relu1 = nn.ELU()
+        self.relu1 = nn.ELU() # inplace=True
         self.dropout1 = nn.Dropout(dropout)
 
-        self.conv2 = Conv1dWithConstraint(n_outputs, n_outputs, kernel_size, stride=stride, padding=padding, 
-                                          dilation=dilation, bias=bias, doWeightNorm=WeightNorm, max_norm=max_norm)
+        if group:
+            self.conv2 = Conv1dWithConstraint(n_outputs, n_outputs, kernel_size, stride=stride, padding=padding, 
+                                              dilation=dilation, bias=bias, doWeightNorm=WeightNorm, max_norm=max_norm, groups=n_outputs)
+            self.conv2_point = Conv1dWithConstraint(n_outputs, n_outputs, kernel_size=1, stride=1, bias=bias)
+        else:
+            self.conv2 = Conv1dWithConstraint(n_outputs, n_outputs, kernel_size, stride=stride, padding=padding, 
+                                              dilation=dilation, bias=bias, doWeightNorm=WeightNorm, max_norm=max_norm)
         self.chomp2 = Chomp1d(padding)
         self.bn2 = nn.BatchNorm1d(num_features=n_outputs)
         self.relu2 = nn.ELU()
         self.dropout2 = nn.Dropout(dropout)
 
-        self.net = nn.Sequential(self.conv1, self.chomp1, self.bn1, self.relu1, self.dropout1,
-                                 self.conv2, self.chomp2, self.bn2, self.relu2, self.dropout2)
+        if group:
+            self.net = nn.Sequential(self.conv1, self.conv1_point, self.chomp1, self.bn1, self.relu1, self.dropout1,
+                                     self.conv2, self.conv2_point, self.chomp2, self.bn2, self.relu2, self.dropout2)
+        else:
+            self.net = nn.Sequential(self.conv1, self.chomp1, self.bn1, self.relu1, self.dropout1,
+                                     self.conv2, self.chomp2, self.bn2, self.relu2, self.dropout2)
         self.downsample = nn.Conv1d(n_inputs, n_outputs, 1) if n_inputs != n_outputs else None
         self.relu = nn.ELU()
-
-    #     self.init_weights()
-
-    # def init_weights(self):
-    #     self.conv1.weight.data.normal_(0, 0.01)
-    #     self.conv2.weight.data.normal_(0, 0.01)
-    #     if self.downsample is not None:
-    #         self.downsample.weight.data.normal_(0, 0.01)
 
     def forward(self, x):
         out = self.net(x)
@@ -71,7 +81,7 @@ class TemporalBlock(nn.Module):
 
 
 class TemporalConvNet(nn.Module):
-    def __init__(self, num_inputs, num_channels, kernel_size=2, dropout=0.2, bias=False, WeightNorm=False, max_norm=1.):
+    def __init__(self, num_inputs, num_channels, kernel_size=2, dropout=0.2, bias=False, WeightNorm=False, group=True, max_norm=1.):
         super(TemporalConvNet, self).__init__()
         layers = []
         num_levels = len(num_channels)
@@ -80,7 +90,7 @@ class TemporalConvNet(nn.Module):
             in_channels = num_inputs if i == 0 else num_channels[i-1]
             out_channels = num_channels[i]
             layers += [TemporalBlock(in_channels, out_channels, kernel_size, stride=1, dilation=dilation_size, padding=(kernel_size-1) * dilation_size, 
-                                     dropout=dropout, bias=bias, WeightNorm=WeightNorm, max_norm=max_norm)]
+                                     dropout=dropout, bias=bias, WeightNorm=WeightNorm, group=group, max_norm=max_norm)]
 
         self.network = nn.Sequential(*layers)
 
